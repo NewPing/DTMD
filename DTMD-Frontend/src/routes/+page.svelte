bc1b8bef656e91d702766c938adb0f87de662ee9
Alex <alexander.lutsch@gmx.de>
2024-07-25 17:43:38 +0200

    center content front page

--- DTMD-Frontend/src/routes/+page.svelte
+++ DTMD-Frontend/src/routes/+page.svelte
@@ -13,22 +13,11 @@
 	};
 </script>
 
-<label for="Name">Name:</label>
 
 <h1>Welcome to DTMD</h1>
-
-<style lang="postcss">
-	h1 {
-		position: relative;
-		text-align: center;
-		font-size: 40px;
-		//font-size: 3vw;
-		top: 5vh;
-	}
-</style>
-
-<button class="btn variant-filled" use:popup={popupFeatured}>Show Popup</button>
-<div class="card p-4 w-72 shadow-xl" data-popup="popupFeatured">
+<div class="centeredFrontPageContent">
+	<button class="btn variant-filled createButton" use:popup={popupFeatured}>Create Room</button>
+	<div class="card p-4 w-72 shadow-xl" data-popup="popupFeatured">
 	<div>
 		<div>Test</div>
 		<div>Test2</div>
@@ -38,3 +27,26 @@
 	</div>
 	<div class="arrow bg-surface-100-800-token" />
 </div>
+
+</div>
+
+
+<style lang="postcss">
+	h1 {
+		position: relative;
+		text-align: center;
+		font-size: 40px;
+		//font-size: 3vw;
+		top: 5vh;
+	}
+	.createButton{
+
+	}
+	.centeredFrontPageContent{
+		height: 100vh;
+    	display: flex;
+    	justify-content: center;
+    	align-items: center;
+	}
+	
+</style>
