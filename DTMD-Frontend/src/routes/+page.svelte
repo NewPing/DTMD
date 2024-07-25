9ecfccd464f1cec333d8bf641f996f8846f4f98d
NewPing <jan-roth1998@web.de>
2024-07-25 18:07:59 +0200

    styling stuff

--- DTMD-Frontend/src/routes/+page.svelte
+++ DTMD-Frontend/src/routes/+page.svelte
@@ -11,19 +11,20 @@
 		// Defines which side of your trigger the popup will appear
 		placement: 'bottom',
 	};
+
+	let roomname = '';
+	let username = '';
 </script>
 
 
 <h1>Welcome to DTMD</h1>
 <div class="centeredFrontPageContent">
-	<button class="btn variant-filled createButton" use:popup={popupFeatured}>Create Room</button>
+	<button class="btn variant-filled createButton" style="margin-top: 10vh; font-size: 20px;" use:popup={popupFeatured}>Create Room</button>
 	<div class="card p-4 w-72 shadow-xl" data-popup="popupFeatured">
-	<div>
-		<div>Test</div>
-		<div>Test2</div>
-		<button class="btn variant-filled" >TestButton</button>
-		<div>test3</div>
-		<p>Demo Content</p>
+	<div class="flex flex-col">
+		<input type="text" bind:value={roomname} placeholder="Room Name">
+		<input type="text" bind:value={username} placeholder="Nickname">
+		<button class="btn variant-filled" style="margin-top: 2vh;">Create</button>
 	</div>
 	<div class="arrow bg-surface-100-800-token" />
 </div>
@@ -33,17 +34,23 @@
 
 <style lang="postcss">
 	h1 {
-		position: relative;
+		margin-top: 6vh;
 		text-align: center;
 		font-size: 40px;
-		//font-size: 3vw;
-		top: 5vh;
 	}
-	.createButton{
+	.createButton {
 
 	}
-	.centeredFrontPageContent{
-		height: 100vh;
+	input {
+        padding: 10px;
+        font-size: 16px;
+        margin: 10px;
+		border-radius: 25px;
+		border-color: transparent;
+		color: black;
+    }
+
+	.centeredFrontPageContent {
     	display: flex;
     	justify-content: center;
     	align-items: center;
